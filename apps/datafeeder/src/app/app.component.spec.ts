7fb1e9602671729031f25057fda4ba9c17b0e0b9
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-05-10 13:54:57 +0200

    feat(header): make df header iframe configurable moving it from index.html to app.component

--- apps/datafeeder/src/app/app.component.spec.ts
+++ apps/datafeeder/src/app/app.component.spec.ts
@@ -1,24 +1,35 @@
 import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { UtilSharedModule } from '@geonetwork-ui/util/shared'
+import SETTINGS from '../settings'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, UtilSharedModule],
       declarations: [AppComponent],
     }).compileComponents()
   })
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy()
-  })
-
-  it(`should have as title 'datafeeder'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('datafeeder')
+  describe('', () => {
+    let fixture
+    let app
+    beforeEach(async () => {
+      fixture = TestBed.createComponent(AppComponent)
+      app = fixture.componentInstance
+    })
+    it('should create the app', () => {
+      expect(app).toBeTruthy()
+    })
+    it(`should have as title 'datafeeder'`, () => {
+      expect(app.title).toEqual('datafeeder')
+    })
+    it(`should have SETTINGS.headerHeight as height`, () => {
+      expect(app.headerHeight).toEqual(SETTINGS.headerHeight)
+    })
+    it(`should have SETTINGS.headerSrc as src`, () => {
+      expect(app.headerSrc).toEqual(SETTINGS.headerSrc)
+    })
   })
 })
